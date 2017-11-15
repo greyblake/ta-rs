43e4d4b3e9b8cdddc5cd92cf4cac84e1ca6aa962
Sergey Potapov <blake131313@gmail.com>
2017-11-15 19:45:45 +0100

    Add MACD doc

--- src/indicators/moving_average_convergence_divergence.rs
+++ src/indicators/moving_average_convergence_divergence.rs
@@ -3,6 +3,50 @@ use {Next, Reset, Close};
 use indicators::ExponentialMovingAverage as Ema;
 use errors::*;
 
+/// Moving average converge divergence (MACD).
+///
+/// The MACD indicator (or "oscillator") is a collection of three time series
+/// calculated from historical price data, most often the closing price.
+/// These three series are:
+///
+/// * The MACD series proper
+/// * The "signal" or "average" series
+/// * The "divergence" series which is the difference between the two
+///
+/// The MACD series is the difference between a "fast" (short period) exponential
+/// moving average (EMA), and a "slow" (longer period) EMA of the price series.
+/// The average series is an EMA of the MACD series itself.
+///
+/// # Formula
+///
+/// # Parameters
+///
+/// * _fast_length_ - length for the fast EMA. Default is 12.
+/// * _slow_length_ - length for the slow EMA. Default is 26.
+/// * _signal_length_ - length for the signal EMA. Default is 9.
+///
+/// # Example
+///
+/// ```
+/// use ta::indicators::MovingAverageConvergenceDivergence as Macd;
+/// use ta::Next;
+///
+/// let mut macd = Macd::new(3, 6, 4).unwrap();
+///
+/// assert_eq!(round(macd.next(2.0)), (0.0, 0.0, 0.0));
+/// assert_eq!(round(macd.next(3.0)), (0.21, 0.09, 0.13));
+/// assert_eq!(round(macd.next(4.2)), (0.52, 0.26, 0.26));
+/// assert_eq!(round(macd.next(7.0)), (1.15, 0.62, 0.54));
+/// assert_eq!(round(macd.next(6.7)), (1.15, 0.83, 0.32));
+/// assert_eq!(round(macd.next(6.5)), (0.94, 0.87, 0.07));
+///
+/// fn round(nums: (f64, f64, f64)) -> (f64, f64, f64) {
+///     let n0 = (nums.0 * 100.0).round() / 100.0;
+///     let n1 = (nums.1 * 100.0).round() / 100.0;
+///     let n2 = (nums.2 * 100.0).round() / 100.0;
+///     (n0, n1, n2)
+/// }
+/// ```
 #[derive(Debug,Clone)]
 pub struct MovingAverageConvergenceDivergence {
     fast_ema: Ema,
