2ae71b0e0f7c26d7a02df54b37b9e6fca2987809
Devin gunay <devingunay@gmail.com>
2020-10-02 18:15:54 -0700

    fix warning when not compiling w/ serde

--- tests/test.rs
+++ tests/test.rs
@@ -5,17 +5,19 @@ extern crate ta;
 
 #[cfg(test)]
 mod test {
-    use ta::indicators::SimpleMovingAverage;
-    use ta::Next;
-
-    // Simple smoke test that serde works (not sure if this is really necessary)
-    #[test]
     #[cfg(feature = "serde_support")]
-    fn test_serde() {
-        let mut macd = SimpleMovingAverage::new(20).unwrap();
-        let bytes = bincode::serialize(&macd).unwrap();
-        let mut deserialized: SimpleMovingAverage = bincode::deserialize(&bytes).unwrap();
+    mod serde {
+        use ta::indicators::SimpleMovingAverage;
+        use ta::Next;
 
-        assert_eq!(deserialized.next(2.0), macd.next(2.0));
+        // Simple smoke test that serde works (not sure if this is really necessary)
+        #[test]
+        fn test_serde() {
+            let mut macd = SimpleMovingAverage::new(20).unwrap();
+            let bytes = bincode::serialize(&macd).unwrap();
+            let mut deserialized: SimpleMovingAverage = bincode::deserialize(&bytes).unwrap();
+
+            assert_eq!(deserialized.next(2.0), macd.next(2.0));
+        }
     }
 }
